9eaf5044a38d28e9f9b65f21f648c72328591b97
Idriss MOHAMADY <i.mohamady@oodrive.com>
2022-10-19 20:26:58 +0200

    add drawer and bottom navigation

--- lib/theme/view/theme_view.dart
+++ lib/theme/view/theme_view.dart
@@ -10,14 +10,18 @@ class ThemeView extends StatelessWidget {
     List<ThemeModel> themeList = context.read<ThemeCubit>().getLightThemes();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bloc Theme'),
+        title: const Text('Theme Preview'),
         actions: [
-          Switch(
-            value: isDark(context),
-            onChanged: (val) async {
-              context.read<ThemeCubit>().changeBrigthness(val);
-            },
-          )
+          Row(
+            children: [
+              Switch(
+                value: isDark(context),
+                onChanged: (val) async {
+                  context.read<ThemeCubit>().changeBrigthness(val);
+                },
+              )
+            ],
+          ),
         ],
       ),
       body: ListView.builder(
@@ -44,6 +48,7 @@ class ThemeView extends StatelessWidget {
           );
         },
       ),
+      drawer: _Drawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context,
@@ -54,6 +59,75 @@ class ThemeView extends StatelessWidget {
           color: Colors.white,
         ),
       ),
+      bottomNavigationBar: _BottomNavigationBar(),
+    );
+  }
+}
+
+class _Drawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+            ),
+            child: const Text(
+              'Drawer header',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          const ListTile(
+            leading: Icon(Icons.message),
+            title: Text('Messages'),
+          ),
+          const ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text('Profile'),
+          ),
+          const ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomNavigationBar extends StatefulWidget {
+  @override
+  _BottomNavigationBarState createState() => _BottomNavigationBarState();
+}
+
+class _BottomNavigationBarState extends State<_BottomNavigationBar> {
+  int _currIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _currIndex,
+      onTap: (index) => setState(() => _currIndex = index),
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.business),
+          label: 'Business',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.school),
+          label: 'School',
+        ),
+      ],
     );
   }
 }
